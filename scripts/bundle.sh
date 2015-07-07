a5cfe9d2a5c36e80115419263aeea1a0460c6927
swagata <swagata.mondal@careerbuilder.com>
2015-07-07 12:14:30 +0530

    new bundle with ubuntu user name

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -4,5 +4,7 @@ rm -rf .bundle
 pwd
 ls
 ruby -v
-bundle --path vendor/bundle
+
+su -l ubuntu -c 'cd /home/ubuntu/codeship_test  && bundle install --path vendor/bundle'
+#bundle --path vendor/bundle
 #cd /home/ubuntu/codeship_test && bundle --path vendor/bundle
